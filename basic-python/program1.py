b2b29d5800401cfdaee34ac6b612a29a7acc26a2
Himanshusinghaiml <himanshusingh945443@gmail.com>
2023-11-08 18:47:30 +0530

    start-again

---
+++ basic-python/program1.py
@@ -0,0 +1,22 @@
+name="himanshu singh"
+print("your name is :",name)
+# if we print something
+for i in range(1,11):
+    print( i,"himanshu singh")
+i =1  
+print("this is printing using while loop ")   
+while(i<11):
+    print(i,"himanshu singh ",end=' @ ') # use in end ,to give space also print on same line
+    # or mention any value 
+    i+=1
+# Using the end parameter to print on the same line
+print("Hello, ", end='')
+print("World!")
+
+# You can also use a different character to separate the printed content
+print("This", end=' ')
+print("is", end=' - ')
+print("on", end=' - ')
+print("the", end=' - ')
+print("same", end=' - ')
+print("line.")
