adaf8edddfa203fb65c2802ff0ee4f66f7b84d00
Himanshusinghaiml <himanshusingh945443@gmail.com>
2023-11-11 12:08:25 +0530

    oops-concepts

---
+++ basic-python/oops-python.py
@@ -0,0 +1,17 @@
+# there are four pillars of python 
+'''
+class
+objects
+encapsulation
+polymorphism
+abstraction
+inheritance 
+ 
+'''
+class aiml:
+    print("himanshu singh") # class function call 
+    def p():
+        print("hello")
+a=aiml()
+ 
+ 
